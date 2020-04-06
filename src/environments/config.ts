5bd93faec654d4562ffc7de2da3c3b5513e3febf
Jason Brown <jsb@iteris.com>
2020-04-06 08:46:37 -0700

    Various package updates

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,7 +1,16 @@
 /* Add your credentials here */
 
-
-
+export const apiCredentials = {
+  CA_User_UUID: 'abacd9f0-ab68-40d2-abf6-659102cc1315',
+  CA_Account_appId: '28f7e06c',
+  CA_Account_appKey:  '8a1e47457dac6bdc2edf4dd74f5d3f05',
+  CA_Account_UUID:  '1c7d76af-a79f-442d-9e38-84f03731f15c',
+  CA_Field_UUID: '28f7a7c1-8caf-4800-ac23-d7ab5ca0df69',
+  CA_Growth_UUID: '3b5de800-6f05-409e-b8a8-429af3854bd9',
+  latitude: 36.608,
+  longitude: -119.824
+};
+/*
 export const apiCredentials = {
     CA_User_UUID: 'GUID',
     CA_Account_appId: 'STRING',
@@ -12,7 +21,7 @@ export const apiCredentials = {
     latitude: number,
     longitude: number
   };
-
+*/
 
 
 
